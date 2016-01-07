63c1d5271dd99f6a8bde001df49ae897a274752e
Youssef Salim <salimyoussef11@yahoo.fr>
2016-01-07 09:27:00 +0100

    Mes changements

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var marks = require('./routes/marks');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/marks', marks);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ backend/routes/marks.js
@@ -0,0 +1,23 @@
+/**
+ * Created by sy306571 on 04/01/16.
+ */
+var express = require('express');
+var router = express.Router();
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+
+/* GET marks listing. */
+router.get('/marks', function(req, res, next) {
+    res.send('Bonjour');
+});
+
+var url = 'mongodb://localhost:27017/test';
+MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+    console.log("Connected correctly to server.");
+    db.close();
+});
+
+
+
+module.exports = router;
