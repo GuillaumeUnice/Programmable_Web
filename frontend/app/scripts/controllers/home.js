03b8558da4677cdcd70ac91c3b0079a3055fe02a
Guillaume BORG <guillaume.unice@gmail.com>
2016-02-04 21:35:54 +0100

    Mise en place du filtre myMix par date de creation et name

--- frontend/app/scripts/controllers/home.js
+++ frontend/app/scripts/controllers/home.js
@@ -2,7 +2,40 @@
 
 angular.module('frontendApp')
   .controller('HomeCtrl', function ($scope, ModalService, searchService,currentMusicService, feedbackService,auth) {
-    $scope.searchInfo = {motsCles: null};
+    
+    $scope.myMix = [
+      { _id: 1,
+        name: "Drop The Pressure",
+        created_at: 1454616841
+      },
+      { _id: 2,
+        name: "Metronome",
+        created_at: 1454616842
+      },
+      { _id: 3,
+        name: "Freaky Voice",
+        created_at: 1454616843
+      },
+      { _id: 6,
+        name: "Turn The World On",
+        created_at: 1454616846
+      },
+      { _id: 4,
+        name: "Close Encounter",
+        created_at: 1454616844
+      },
+      { _id: 5,
+        name: "Pumped Up Kicks",
+        created_at: 1454616845
+      },
+      { _id: 7,
+        name: "Turn The World On 2",
+        created_at: 1454616847
+      },
+    ];
+
+
+    /*$scope.searchInfo = {motsCles: null};
     $scope.results = [];
     $scope.chosenMusic = null;
 
@@ -53,6 +86,7 @@ angular.module('frontendApp')
           modal.element.modal();
           modal.close.then(function(){});
         });
-    };
+    };*/
 
-  });
+
+});
