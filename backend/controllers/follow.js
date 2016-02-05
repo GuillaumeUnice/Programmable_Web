cc055b0044a0535c5ce3512274f179c20b28e529
Youssef Salim <salimyoussef11@yahoo.fr>
2016-02-06 00:37:14 +0100

    Follow remis à jour

--- backend/controllers/follow.js
+++ backend/controllers/follow.js
@@ -3,32 +3,34 @@ var usersRepository = new usersRepositoryModule.UsersRepository();
 
 exports.followSomeone = function(req,res){
     var idUser = req.body.idUser;
-    var idFollowed = req.body.idFollowed;
+    var idfollowing = req.body.idFollowing;
 
     var user;
-    var followed;
+    var following;
     usersRepository.findUserById(req.db,idUser,function(err,result){
         if(result==null){
             res.send(404,'User not found')
         }
         else{
             user = result;
-            for(var i = 0; i<user.followed.length;i++){
-                if(user.followed[i]==idFollowed){
+            for(var i = 0; i<user.following.length;i++){
+                if(user.following[i]._id==idfollowing){
                     res.send(404,'User already following the other user');
                     return;
                 }
             }
 
-            usersRepository.findUserById(req.db,idFollowed,function(err,result){
+            usersRepository.findUserById(req.db,idfollowing,function(err,result){
                 if(result==null){
-                    res.send(404,'The followed user not found')
+                    res.send(404,'The user to follow not found')
                 }
                 else{
-                    followed = result;
-                    req.db.collection('users').updateOne({_id : idUser},{ $push: { followed: idFollowed } });
-                    req.db.collection('users').updateOne({_id : idFollowed},{ $push: { followers: idUser } });
-                    var event = {describ: user.full_name+" is now following "+followed.full_name,
+                    following = result;
+                    req.db.collection('users').updateOne({_id : idUser},{ $push: { following: {_id: idfollowing,
+                    full_name: following.full_name} } });
+                    req.db.collection('users').updateOne({_id : idfollowing},{ $push: { followers: {_id: idUser,
+                    full_name: user.full_name} } });
+                    var event = {describ: user.full_name+" is now following "+following.full_name,
                         created_at: new Date().getTime()};
                     usersRepository.notifyFollowers(req.db,idUser,event,
                      function(){
@@ -43,12 +45,12 @@ exports.followSomeone = function(req,res){
     });
 };
 
-exports.getFollowed = function(req,res){
+exports.getFollowing = function(req,res){
     req.db.collection('users').findOne({_id : req.params.idUser },function(err,doc){
         if(doc==null){
             res.send(404,'This user doesn\'t exist');
         }
-        else res.send(doc.followed);
+        else res.send(doc.following);
     });
 };
 
