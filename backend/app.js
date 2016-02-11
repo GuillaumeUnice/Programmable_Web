1d43f4aa410b07727cef2cad76175a13be086213
Youssef Salim <salimyoussef11@yahoo.fr>
2016-02-11 15:14:01 +0100

    Migration des routes dans index.js

--- backend/app.js
+++ backend/app.js
@@ -16,14 +16,6 @@ var jwtMid = require('express-jwt');
 var bcrypt = require('bcryptjs');
 var salt = bcrypt.genSaltSync(10);
 
-/** TODO REMOVE **/
-//var session = require('express-session');
-
-//var routes = require('./routes/index');
-//var users = require('./routes/users');
-//var feedbacks = require('./routes/feedbacks');
-//var search = require('./routes/search');
-
 var app = express();app.use(busboy());
 
 var constants = require('./config/constants');
@@ -58,13 +50,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 //Routes
-var routess = {};
-routess.auth = require('./controllers/auth.js');
-routess.search = require('./controllers/search.js');
-routess.feedbacks = require('./controllers/feedbacks.js');
-routess.follow = require('./controllers/follow.js');
-routess.manageMySongs = require('./controllers/manageMySongs.js');
-routess.account = require('./controllers/account.js');
+var routes = require('./routes/index.js');
 
 
 app.use(function(req, res, next) {
@@ -79,51 +65,7 @@ app.use(function(req, res, next) {
 });
 
 
-/**********************************************************************************
- *                Route for auth API
- **********************************************************************************/
-app.post('/register', routess.auth.register);
-app.post('/login', routess.auth.login);
-app.post('/logout', routess.auth.logout);
-
-app.get('/feedbacks/:idSong', routess.feedbacks.getFeedbacks);
-
-app.get('/mix/:idMix', authMiddelware.ensureAuthorized, routess.feedbacks.getMix);
-app.post('/comment', authMiddelware.ensureAuthorized, routess.feedbacks.postFeedback);
-app.post('/mark', authMiddelware.ensureAuthorized, routess.feedbacks.postMark);
-app.post('/search', authMiddelware.ensureAuthorized, routess.search.searchSongAndUser);
-
-app.post('/follow',authMiddelware.ensureAuthorized,routess.follow.followSomeone);
-app.get('/follow/followers/:idUser',routess.follow.getFollowers);
-app.get('/follow/following/:idUser',routess.follow.getFollowing);
-app.post('/unfollow',authMiddelware.ensureAuthorized,routess.follow.unfollow);
-
-app.get('/manageMySongs/:idUser',authMiddelware.ensureAuthorized,routess.manageMySongs.getMySongs);
-app.get('/manageMySongs/:idUser',authMiddelware.ensureAuthorized,routess.manageMySongs.getMySongs2);
-
-app.get('/account/:idUser',authMiddelware.ensureAuthorized,routess.account.getAccountInfo);
-
-//Functions previously used in index
-app.post('/save',routess.manageMySongs.save);
-app.post('/upload',routess.manageMySongs.upload);
-app.post('/download',routess.manageMySongs.download);
-app.get('/track',routess.manageMySongs.getTracks);
-app.get('/track/:id',routess.manageMySongs.getTrackById);
-app.post('/mixed',routess.manageMySongs.uploadMixed);
-app.post('/savemixed',routess.manageMySongs.savemixed);
-app.get('/getmixed',routess.manageMySongs.getMixed);
-app.get('/getMixedSongInfo',routess.manageMySongs.getMixedSongInfo);
-app.get('/folderName',routess.manageMySongs.getFolderName);
-app.get('/get',routess.manageMySongs.getSongsByName);
-
-var parth = /\/track\/(\w+)\/(?:sound|visualisation)\/((\w|.)+)/;
-app.get(parth,routess.manageMySongs.uploadSongs);
-
-
-//app.use('/', routes);
-//app.use('/users', users);
-//app.use('/feedbacks', feedbacks);
-//app.use('/search', search);
+app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
